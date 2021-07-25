b4d58a44bc0651a76910cc1aaf6ffc21b3dbd9a3
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2021-07-25 19:49:28 +0300

    Kommit

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const galleryItems = [
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
+      imageEl:
       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
     description: 'Nature Landscape',
   },
@@ -66,48 +66,78 @@ const galleryItems = [
 
 
 
+// гроздь ul-li-img
+// const liEl = document.createElement('li');
+// liEl.classList.add('site_nav');
 
-// import galleryItems from "./app.js";
+// const imageEl = document.createElement('img');
+// imageEl.classList.add('img-gallery');
+// liEl.appendChild(imageEl);
 
-const refs = {
-  mainGallery: document.querySelector(".js-gallery"),
-  lightBoxEl: document.querySelector(".js-lightbox"),
-  modal: document.querySelector(".lightboxcontent"),
-  lightboxImage: document.querySelector(".lightboximage"),
-  btnLightBox: document.querySelector('[data-action="close-lightbox"]'),
-  overlayEl: document.querySelector(".lightboxoverlay"),
-};
+// const ulEl = document.querySelector('.gallery');
+// console.log(ulEl);
 
-// refs.mainGallery.addEventListener("click", isGalleryImage);
-// refs.btnLightBox.addEventListener("click", closeLightBoxWindow);
-// refs.modal.addEventListener("click", closeLightBoxImage);
-// refs.overlayEl.addEventListener("click", onOverlayClose);
+// ulEl.appendChild(liEl);
 
-// сoздаю пример карточки
-function createImgCardMarkup(galleryItems) {
-  return galleryItems
-  .map(({ preview, original, description }, index) => {
-      return `<li class="galleryitem >
-      <a
-        class="gallerylink"
-        href="${original}"
-      >
-        <img
-          class="galleryimage"
-          src="${preview}"
-          data-source="${original}" 
-          alt="${description}"
-          data-index="${index}"
-        />
-      </a>
-    </li>`;
-    })
-    .join("");
+
+const ulEl = document.querySelector('.js-gallery');
+const elements = [];
+for (let i = 0; i < galleryItems.length; i+= 1) {
+  const liEl = document.createElement('li');
+  const imageEl = document.createElement('img');
+  liEl.appendChild(imageEl);
+imageEl.src = galleryItems[i].preview
+  elements.push(liEl);
 }
 
+console.log(elements);
+ulEl.append(...elements);
 
-const cardsItem = createImgCardMarkup(galleryItems); // вивел всею разметку в карточки
-console.log(cardsItem);
 
-refs.mainGallery.insertAdjacentHTML("beforeend", cardsItem); // вставил разметку
-// console.log(mainGalery);
+
+// Пєтя
+// import galleryItems from "./app.js";
+
+// const refs = {
+//   mainGallery: document.querySelector(".js-gallery"),
+//   lightBoxEl: document.querySelector(".js-lightbox"),
+//   modal: document.querySelector(".lightboxcontent"),
+//   lightboxImage: document.querySelector(".lightboximage"),
+//   btnLightBox: document.querySelector('[data-action="close-lightbox"]'),
+//   overlayEl: document.querySelector(".lightboxoverlay"),
+// };
+
+// // refs.mainGallery.addEventListener("click", isGalleryImage);
+// // refs.btnLightBox.addEventListener("click", closeLightBoxWindow);
+// // refs.modal.addEventListener("click", closeLightBoxImage);
+// // refs.overlayEl.addEventListener("click", onOverlayClose);
+
+// // сoздаю пример карточки
+// function createImgCardMarkup(galleryItems) {
+//   return galleryItems
+//   .map(({ preview, original, description }, index) => {
+//       return `<li class="galleryitem >
+//       <a
+//         class="gallerylink"
+//         href="${original}"
+//       >
+//         <img
+//           class="galleryimage"
+//           src="${preview}"
+//           data-source="${original}" 
+//           alt="${description}"
+//           data-index="${index}"
+//         />
+//       </a>
+//     </li>`;
+//     })
+//     .join("");
+// }
+
+
+// const cardsItem = createImgCardMarkup(galleryItems); // вивел всею разметку в карточки
+// console.log(cardsItem);
+
+// refs.mainGallery.insertAdjacentHTML("beforeend", cardsItem); // вставил разметку
+// // console.log(mainGalery);
+
